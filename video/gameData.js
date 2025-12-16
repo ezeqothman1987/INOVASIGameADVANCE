bc90340f9772bdd8d44fcb39fc918324a6a6a288
ezeqothman1987 <ezeq.othman@jmg.gov.my>
2025-12-16 09:45:38 +0800

    Update gameData.js

--- video/gameData.js
+++ video/gameData.js
@@ -37,9 +37,9 @@ const UI_TEXT = {
    - Boleh tambah batu / topik
 ========================= */
 const QUESTION_BANK = {
-  Granit: [
-    { q: "Granit ialah batuan igneus?", a: true },
-    { q: "Granit terbentuk di permukaan bumi?", a: false }
+  Coal: [
+    { q: "Batuan ini merupakan antara sumber utama tenaga bahan api di dunia.", a: true },
+    { q: "Batuan ini tergolong dalam batuan igneus.", a: false }
   ],
 
   Syis: [
@@ -47,27 +47,52 @@ const QUESTION_BANK = {
     { q: "Syis terbentuk dari lava?", a: false }
   ],
 
-  Kuarzit: [
-    { q: "Kuarzit berasal dari batu pasir?", a: true },
-    { q: "Kuarzit ialah batuan igneus?", a: false }
+  Kuarza: [
+    { q: "Mineral ini merupakan mineral paling banyak di kerak bumi", a: true },
+    { q: "Mineral ini digunakan dalam pembuatan utama senjata api, cawan plastik & kertas.", a: false }
   ],
    
-  Pegmatit: [
-    { q: "Pegmatit ialah batuan igneus.", a: true },
-    { q: "Pegmatit terbentuk daripada pemendapan sedimen.", a: false }
+  Serpentinite: [
+    { q: "Batuan ini tergolong dalam batuan metamorfik.", a: true },
+    { q: "Nama batuan ini ialah Serpentinite.", a: true }
   ],
    
-  BatuKapur: [
-    { q: "Batu kapur ialah batuan sedimen.", a: true },
-    { q: "Batu kapur terbentuk daripada lava gunung berapi.", a: false }
+  Muskovit: [
+    { q: "Mineral ini mempunyai sifat berkeping dan berwarna hitam", a: false },
+    { q: "Mineral ini tergolong dalam kumpulan mika", a: false }
+  ],  
+   
+   REE: [
+    { q: "REE karbonat mengandungi mineral emas dan bijih timah.", a: false },
+    { q: "REE digunakan dalam pembuatan Telefon pintar, tablet, kenderaan elektrik, turbin angin dan panel solar.", a: true }
+  ],   
+   
+   Granit: [
+    { q: "Mineral biasa yang terkandung dalam batuan ini ialah kuarza, alkali feldspar dan biotit.", a: true },
+    { q: "Lesung batu dan meja dapur diperbuat daripada batuan ini.", a: true }
+  ],   
+   
+   Syal: [
+    { q: "Debu hitam dalam batuan ini disebabkan oleh kandungan karbon (grafit) yang tinggi.", a: true },
+    { q: "Batuan ini merupakan batuan sedimen berbutir halus.", a: true }
+  ],   
+   
+   Cassiterite: [
+    { q: "Nama lain bagi mineral ini ialah BIJIH TIMAH.", a: true },
+    { q: "Terdapat lombong mineral ini di Negeri Sembilan.", a: true }
+  ],   
+   
+   Chalcopyrite: [
+    { q: "Chalcopyrite juga dikenali sebagai fool’s gold.", a: true },
+    { q: "Mineral ini merupakan sumber TEMBAGA.", a: true }
   ],
    
   Gneiss: [
-    { q: "Gneiss ialah batuan metamorf.", a: true },
-    { q: "Gneiss terbentuk daripada batuan sedimen tanpa tekanan.", a: false }
+    { q: "Batuan ini terbentuk melalui proses metamorfisme sentuhan.", a: false },
+    { q: "Nama batuan ini ialah gneiss", a: true }
   ]
 
-  // 🔒 Simpan untuk masa depan
+  // Simpan untuk masa depan
   // Basalt: [],
   // Gneiss: [],
   // Marble: []
